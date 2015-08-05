822f96e4f136e837daed0f3f8cf09efd572976fd
Adam Knox <adam.w.knox@gmail.com>
2015-08-05 18:13:01 -0400

    user props

--- build/js/app.js
+++ build/js/app.js
@@ -1,22 +1,34 @@
 angular.module('app',[
-  // Angular Team
-  'ngRoute',
+	// Angular Team
+	'ngRoute',
 
-  // Third Party
-  'angular-stamplay',
+	// Third Party
+	'angular-stamplay',
 
-  // Local
-  'UserService',
-  'home'
+	// Local
+	'UserService',
+	'home'
 ])
 .config(function($locationProvider) {
-  // $locationProvider.html5Mode(true);
+	// $locationProvider.html5Mode(true);
 })
 .controller('appController', function($scope, User){
-  User.getCurrent()
-      .then(function(resp){
-          console.log(resp);
-      })
+	var app = this;
+
+	app.user = {};
+
+	User.getCurrent()
+		.then(function(data){
+			console.log(data.get('_id'));
+			if (data.get('_id')) {
+				app.user.isLoggedIn = true;
+				app.user.id = data.get('_id');
+				app.user.name = data.get('displayName');
+				app.user.img = data.get('profileImg');
+			} else {
+				app.user.isLoggedIn = false;
+			}
+		})
 });
 (function(){
 
