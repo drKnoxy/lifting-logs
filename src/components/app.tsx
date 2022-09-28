4b1b0e44ccd3d1fcf7a99d076d375838da558697
Adam Willoughby-Knox <adam.w.knox@gmail.com>
2022-09-28 00:56:38 +0000

    more conversion

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,9 +1,13 @@
-import { Fragment, h } from "preact";
+import cx from "classnames";
+import { h } from "preact";
 import { useState } from "preact/hooks";
 
 const App = () => {
   const [alert, setAlert] = useState<false | string[]>();
   const [isEditMode, setEditMode] = useState<boolean>();
+
+  const [week, setCurrentWeek] = useState<number>(0);
+  const [cycle, setCurrentCycle] = useState<number>(0);
   const [records, setRecords] = useState<
     Array<{ label: string; reps: number; weight: number }>
   >([]);
@@ -11,20 +15,7 @@ const App = () => {
   return (
     <div className="container">
       {alert && alert.length && (
-        <div
-          style="position: fixed; left: 15px; right: 15px; z-index: 100;"
-          className="alert alert-info ng-hide"
-        >
-          <a onClick={() => setAlert(false)} className="close">
-            x
-          </a>
-
-          <ul>
-            {alert.map((a) => (
-              <li>{a}</li>
-            ))}
-          </ul>
-        </div>
+        <AlertInfo alert={alert} onCloseClick={() => setAlert(false)} />
       )}
 
       <div>
@@ -49,63 +40,9 @@ const App = () => {
                 </a>
               )}
             </h2>
-            {/* <form name="recordForm"
-                  ng-show="app.showEditForm">
-                  <div className="card">
-                      <div className="card-block">
-                          <div ng-repeat="record in app.$storage.records">
-                              <div className="form-inline">
-                                  <div className="row">
-                                      <div className="col-sm-3">
-                                          <label className="hidden-xs">{{record.label}}</label>
-                                          <h4 className="visible-xs">{{record.label}}</h4>
-                                      </div>
+            {isEditMode && <EditForm />}
 
-                                      <div class="col-sm-9">
-                                          <div class="row">
-                                              <div class="col-xs-6">
-                                                  <fieldset class="form-group">
-                                                      <label class="visible-xs">Reps</label>
-                                                      <input class="form-control"
-                                                          id=""
-                                                          type="number"
-                                                          ng-model="record.reps"
-                                                          required />
-                                                  </fieldset>
-                                                  <p class="form-control-static hidden-xs">x</p>
-                                              </div>
-                                              <div class="col-xs-6">
-                                                  <fieldset class="form-group">
-                                                      <label class="visible-xs">Weight</label>
-                                                      <div class="input-group">
-                                                          <input class="form-control"
-                                                              id=""
-                                                              type="number"
-                                                              ng-model="record.weight"
-                                                              required />
-                                                          <div class="input-group-addon">
-                                                              lbs
-                                                          </div>
-                                                      </div>
-                                                  </fieldset>
-                                              </div>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-
-                          <a class="btn btn-primary"
-                              ng-disabled="recordForm.$invalid"
-                              ng-click="app.showEditForm = false">
-                              <i class="fa fa-check"></i>
-                              Done
-                          </a>
-
-                      </div>
-                  </div>
-              </form> */}
-            {isEditMode && (
+            {!isEditMode && (
               <div className="row">
                 {records.map((record) => {
                   return (
@@ -116,13 +53,13 @@ const App = () => {
                           {record.reps} x {record.weight}
                         </span>
                         {record.reps > 1 && (
-                          <i ng-show="record.reps > 1">
+                          <i>
                             {/* TODO */}
-                            {/* (1RM x {{app.oneRepMax(record) | round5}}) } */}
+                            {/* (1RM x {app.oneRepMax(record) | round5}}) } */}
                           </i>
                         )}
                       </div>
-                      <div class="col-xs-12">
+                      <div className="col-xs-12">
                         90% of your 1rm is used as the base for calculations.
                       </div>
                     </div>
@@ -133,49 +70,32 @@ const App = () => {
           </div>
         </div>
       </div>
+      <hr className="m-y-1" />
+
+      <NavBar
+        cycle={cycle}
+        setCycle={setCurrentCycle}
+        week={week}
+        setWeek={setCurrentWeek}
+      />
+
       {/* 
-  <hr class="m-y-1" />
 
-  <div class="m-b-1 hidden-print">
-      <div class="header-buttons">
-          <label>cycle:</label>
-          <div class="btn-group">
-              <a ng-repeat="cycle in [0,1,2,3,4,5,6,7]"
-                  ng-click="app.$storage.currentCycle = cycle"
-                  class="btn btn-default"
-                  ng-class="{active: (cycle == app.$storage.currentCycle)}">
-                  {{ cycle+1 }}
-              </a>
-          </div>
-      </div>
-      <div class="header-buttons" style="clear:both;margin-bottom: 4px;">
-          <label>week:</label>
-          <div class="btn-group">
-              <a ng-repeat="week in [0,1,2,3]"
-                  ng-click="app.$storage.currentWeek = week"
-                  class="btn btn-default"
-                  ng-class="{active: (week == app.$storage.currentWeek)}">
-                  {{ week+1 }}
-              </a>
-          </div>
-      </div>
 
-      <h2 class="display-2">Lifting Routine</h2>
-  </div>
 
-  <div class="m-b-1 xs-expand">
-      <table class="table table-bordered table-program"
+  <div className="m-b-1 xs-expand">
+      <table className="table table-bordered table-program"
           ng-repeat="week in [0,1,2,3]"
-          ng-class="{active: week == app.$storage.currentWeek}">
+          ng-className="{active: week == app.$storage.currentWeek}">
           <thead>
               <tr>
                   <th colspan="99"
-                      class="table-header">
+                      className="table-header">
                       Week {{ week + 1 }} / Cycle {{ app.$storage.currentCycle + 1 }}
                   </th>
               </tr>
               <tr>
-                  <th class="hidden-sm-down">&nbsp;</th>
+                  <th className="hidden-sm-down">&nbsp;</th>
                   <th ng-repeat="record in app.$storage.records">
                       {{record.label}}
                   </th>
@@ -183,32 +103,32 @@ const App = () => {
           </thead>
           <tbody>
               <tr ng-repeat="set in app.getSets(week)"
-                  ng-class="{ 'active' : $last}">
-                  <th class="hidden-sm-down row-label"
+                  ng-className="{ 'active' : $last}">
+                  <th className="hidden-sm-down row-label"
                       rowspan="3"
                       ng-if="$first || $index === 3">
                       {{ ($first) ? 'Warm Up' : 'Active' }}
                   </th>
                   <td
                       ng-repeat="record in app.$storage.records"
-                      class="nowrap"
+                      className="nowrap"
                       ng-click="app.isAlertVisible = true; app.alertMessages = app.getPlates(app.calcWeight(app.oneRepMax(record), set.percentage, app.$storage.currentCycle, record.increment))"
                   >
-                      <span ng-class="{'print-50': $parent.$last}">
-                          <span class="calculated-weight">
+                      <span ng-className="{'print-50': $parent.$last}">
+                          <span className="calculated-weight">
                               {{ app.calcWeight(app.oneRepMax(record), set.percentage, app.$storage.currentCycle, record.increment) }}
                           </span>
-                          <span class="separator--weight-and-reps">x</span>
-                          <span class="reps">{{set.reps}}</span>
+                          <span className="separator--weight-and-reps">x</span>
+                          <span className="reps">{{set.reps}}</span>
                       </span>
-                      <span ng-if="$parent.$last" class="print-pipe"></span>
-                      <span ng-if="$parent.$last" class="print-50"></span>
+                      <span ng-if="$parent.$last" className="print-pipe"></span>
+                      <span ng-if="$parent.$last" className="print-50"></span>
                   </td>
               </tr>
           </tbody>
           <tbody>
               <tr>
-                  <th class="hidden-sm-down"
+                  <th className="hidden-sm-down"
                       rowspan="2">
                       Accessory
                   </th>
@@ -224,19 +144,19 @@ const App = () => {
               </tr>
               <tr>
                   <td>
-                      Chin-ups <span class="hidden-sm-down">-</span>
+                      Chin-ups <span className="hidden-sm-down">-</span>
                       5 x 10
                   </td>
                   <td>
-                      Hanging Leg Raise <span class="hidden-sm-down">-</span>
+                      Hanging Leg Raise <span className="hidden-sm-down">-</span>
                       5 x 10
                   </td>
                   <td>
-                      Dumbbell Row <span class="hidden-sm-down">-</span>
+                      Dumbbell Row <span className="hidden-sm-down">-</span>
                       5 x 10
                   </td>
                   <td>
-                      Leg Curl <span class="hidden-sm-down">-</span>
+                      Leg Curl <span className="hidden-sm-down">-</span>
                       5 x 10
                   </td>
               </tr>
@@ -253,3 +173,137 @@ const App = () => {
 };
 
 export default App;
+function AlertInfo({
+  onCloseClick,
+  alert,
+}: {
+  onCloseClick: () => void;
+  alert: string[];
+}) {
+  return (
+    <div
+      style="position: fixed; left: 15px; right: 15px; z-index: 100;"
+      className="alert alert-info ng-hide"
+    >
+      <a onClick={onCloseClick} className="close">
+        x
+      </a>
+
+      <ul>
+        {alert.map((a) => (
+          <li>{a}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function EditForm({
+  onClose,
+  records,
+}: {
+  records: { label: string }[];
+  onClose: () => void;
+}) {
+  return (
+    <form>
+      <div className="card">
+        <div className="card-block">
+          {records.map((record) => {
+            return (
+              <div>
+                <div className="form-inline">
+                  <div className="row">
+                    <div className="col-sm-3">
+                      <label className="hidden-xs">{record.label}</label>
+                      <h4 className="visible-xs">{record.label}</h4>
+                    </div>
+
+                    <div className="col-sm-9">
+                      <div className="row">
+                        <div className="col-xs-6">
+                          <fieldset className="form-group">
+                            <label className="visible-xs">Reps</label>
+                            <input
+                              className="form-control"
+                              type="number"
+                              //   ng-model="record.reps"
+                              required
+                            />
+                          </fieldset>
+                          <p className="form-control-static hidden-xs">x</p>
+                        </div>
+                        <div className="col-xs-6">
+                          <fieldset className="form-group">
+                            <label className="visible-xs">Weight</label>
+                            <div className="input-group">
+                              <input
+                                className="form-control"
+                                type="number"
+                                //   ng-model="record.weight"
+                                required
+                              />
+                              <div className="input-group-addon">lbs</div>
+                            </div>
+                          </fieldset>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+
+          <a className="btn btn-primary" onClick={onClose}>
+            <i className="fa fa-check"></i>
+            Done
+          </a>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+type NavBarProps = {
+  cycle: number;
+  setCycle: (n: number) => void;
+  week: number;
+  setWeek: (n: number) => void;
+};
+function NavBar(props: NavBarProps) {
+  return (
+    <div className="m-b-1 hidden-print">
+      <div className="header-buttons">
+        <label>cycle:</label>
+        <div className="btn-group">
+          {[0, 1, 2, 3, 4, 5, 6, 7].map((cycle) => (
+            <a
+              className={cx("btn btn-default", {
+                active: cycle == props.cycle,
+              })}
+              onClick={() => props.setCycle(cycle)}
+            >
+              {props.cycle + 1}
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className="header-buttons" style="clear:both;margin-bottom: 4px;">
+        <label>week:</label>
+        <div className="btn-group">
+          {[0, 1, 2, 3].map((week) => (
+            <a
+              className={cx("btn btn-default", { active: week === props.week })}
+              onClick={() => props.setWeek(week)}
+            >
+              {props.week + 1}
+            </a>
+          ))}
+        </div>
+      </div>
+
+      <h2 className="display-2">Lifting Routine</h2>
+    </div>
+  );
+}
